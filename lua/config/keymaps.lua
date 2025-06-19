58887d8eed610001bc08df76754ca4a54717adae
Alex Kranias <alexander.kranias@gmail.com>
2025-06-19 11:48:30 -0700

    feat: keybindings and terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,19 @@
 vim.g.mapleader = " "
 
+-- Copy and Paste to System Clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- clipboard yank selected lines
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p') -- clipboard paste
+
+-- Terminal
+vim.keymap.set('n', '<leader>t', ':bel split | terminal<CR>', { desc = 'Open terminal (split)' })
+vim.keymap.set('n', '<leader>T', ':rightbelow vsplit | terminal<CR>', { desc = 'Open terminal (vsplit)' })
+vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true }) -- still need to do :q after
+
+-- File Explorer
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle File Explorer' })
+vim.keymap.set('n', '<leader>f', ':NvimTreeFocus<CR>', { desc = 'Focus File Explorer' })
+
+-- Telescope (Fuzzy File Finder)
+vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>", { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', { desc = 'Live Grep' })
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,34 @@ return {
   { "folke/lazy.nvim" },
 
   -- File explorer
-  { "nvim-tree/nvim-tree.lua" },
+  {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    -- Disable netrw
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    -- Setup NvimTree
+    require("nvim-tree").setup({
+      hijack_netrw = true,
+      hijack_cursor = true,
+      sync_root_with_cwd = true,
+      view = {
+        width = 30,
+        side = "left",
+        preserve_window_proportions = true,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = false,
+        },
+      },
+    })
+  end,
+  },
 
   -- Fuzzy finder
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
